5d39041a1749e62f38d6ed31adcfb8af1aff929e
Agus Narvaez <agus.narvaez@outlook.com>
2023-06-27 16:42:47 -0300

    Creo las peticiones de autorización

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
+import credentials from '../credentials'
+const API = credentials.api.testUrl
 
-const API = 'http://localhost:3030/api/auth'
-
+//* Login and Register requests
 const loginRequest = user => axios.post(`${API}/login`, user)
-
 const registerRequest = user => axios.post(`${API}/register`, user)
 
 export { loginRequest, registerRequest}
