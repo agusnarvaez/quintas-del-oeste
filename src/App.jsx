8451f4e124c076e8001e7b68c7a12cfedc5a5c09
Agus Narvaez <agus.narvaez@outlook.com>
2023-06-27 16:48:26 -0300

    Englobo la aplicación en AuthProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Suspense, lazy } from "react"
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-
+import { AuthProvider } from "./context/AuthContext"
 const Home = lazy(()=>import("./pages/Home"))
 const Admin = lazy(()=>import("./pages/Admin"))
 const Login = lazy(()=>import("./pages/Login"))
@@ -11,17 +11,19 @@ const Error404 = lazy(()=>import("./pages/ErrorNotFound"))
 export default function App() {
   return (
     <Suspense fallback={<div className="loading">Cargando...</div>}>
-      <BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/admin/login" element={<Login />} />
-          <Route path="/admin/register" element={<Register />} />
-          <Route path="*" element={<Error404 />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+              <Route path="/admin" element={<Admin />} />
+              <Route path="/admin/login" element={<Login />} />
+              <Route path="/admin/register" element={<Register />} />
+            <Route path="*" element={<Error404 />} />
+          </Routes>
 
-      </BrowserRouter>
+        </BrowserRouter>
+      </AuthProvider>
     </Suspense>
   )
 }
