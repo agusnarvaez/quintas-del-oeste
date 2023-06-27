5f406427587f10e5b8b5dbdfaec030cfc14e4e85
Agus Narvaez <agus.narvaez@outlook.com>
2023-06-27 19:50:14 -0300

    Agrego función para loguearse

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,11 +1,12 @@
 import { createContext, useState, useContext } from "react"
-import { registerRequest } from '../api/auth'
+import { loginRequest, registerRequest } from '../api/auth'
 
 //* Creo el contexto de autenticación para poder usarlo en cualquier parte de la aplicación
 export const AuthContext = createContext()
 
 //* Creo un hook para poder usar el contexto de autenticación en cualquier parte de la aplicación
 export const useAuth = () => {
+    //* Obtengo el contexto de autenticación desde el proveedor AuthContext
     const context = useContext(AuthContext)
     if(!context) throw new Error("useAuth debe estar dentro del proveedor AuthContext")
     return context
@@ -16,7 +17,6 @@ export const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null)
     const [isAuthenticated, setIsAuthenticated] = useState(false)
     const [errors,setErrors] = useState([])
-
 
     const signUp = async (user) => {
         try{
@@ -29,13 +29,23 @@ export const AuthProvider = ({children}) => {
             console.log(error)
             setErrors(error.response.data.errors)
         }
+    }
 
+    const signIn = async (user) => {
+        try{
+            const repsonse = await loginRequest(user)
+            setUser(repsonse.data)
+            setIsAuthenticated(true)
+        }catch(error){
+            setErrors(error.response.data.errors)
+        }
     }
 
     return (
         <AuthContext.Provider
             value={{
                 signUp,
+                signIn,
                 user,
                 isAuthenticated,
                 errors
