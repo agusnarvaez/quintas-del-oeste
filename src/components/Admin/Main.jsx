b540d1c119bf2d78480dd55e3afcde5a151ddf5c
Agus Narvaez <agus.narvaez@outlook.com>
2023-06-29 18:06:05 -0300

    Agrego formulario y lista de lotes

--- src/components/Admin/Main.jsx
+++ src/components/Admin/Main.jsx
@@ -1,8 +1,12 @@
-
+import LotForm from "./LotForm"
+import LotsList from "./LotsList"
 export default function Main() {
   return (
-    <main className="bg-dark-subtle">
-        Admin Main
+    <main className="bg-dark-subtle col-12 m-0 row justify-content-center">
+
+        <LotForm/>
+
+        <LotsList/>
     </main>
   )
 }
