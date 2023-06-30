cdca21edd92cb9d8adb174cd49ae7b176858e7f5
Agus Narvaez <agus.narvaez@outlook.com>
2023-06-30 19:23:03 -0300

    Agrego componente de formulario de reserva

---
+++ src/pages/ReservationForm.jsx
@@ -0,0 +1,9 @@
+
+export default function ReservationForm() {
+  return (
+    <main className="container-fluid p-0">
+      <h1>Form</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam corporis vitae laborum cumque? Quas veritatis, eligendi provident labore inventore et temporibus aliquid quia expedita voluptatum obcaecati minima ut cupiditate!</p>
+    </main>
+  )
+}
