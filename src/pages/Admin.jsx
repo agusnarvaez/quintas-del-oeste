6b1062850580e08472a90ae7417e931a9e32f329
Agus Narvaez <agus.narvaez@outlook.com>
2023-07-02 18:56:22 -0300

    Renombro header de Admin

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,11 +1,12 @@
+import '../assets/styles/adminPanel.css'
 import Main from "../components/Admin/Main"
 import Footer from "../components/Admin/Footer"
-import Header from "../components/Admin/Header"
+import Header from "../components/Admin/AdminHeader"
 import { useAuth } from '../context/AuthContext'
 
 export default function Admin() {
   const {user} = useAuth()
-  
+
   return (
     <>
       <Header id="header" user={user} />
