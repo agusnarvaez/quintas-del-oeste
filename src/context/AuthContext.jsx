bf2a598a9de8934f89f42123c74e7dbf41ff0688
Agus Narvaez <agus.narvaez@outlook.com>
2023-07-02 18:55:25 -0300

    Corrijo respuesta de signUp

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -21,8 +21,8 @@ export const AuthProvider = ({children}) => {
     const signUp = async (user) => {
         try{
             const response = await registerRequest(user)
-
-            setUser(response.data)
+            console.log(response)
+            setUser(response.data.user)
             setIsAuthenticated(true)
         }catch(error){
             console.log(error)
