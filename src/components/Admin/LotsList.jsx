c7f1ae3dabd8b698ef56a4596c45c02e3b80fcb6
Agus Narvaez <agus.narvaez@outlook.com>
2023-07-04 18:51:04 -0300

    Agrego manzana a listado de lotes

--- src/components/Admin/LotsList.jsx
+++ src/components/Admin/LotsList.jsx
@@ -30,15 +30,16 @@ export default function LotsList({setEditionForm,setShowForm}) {
     <section className="lotsList text-admin-primary bg-admin-primary overflow-x-scroll col-12 m-0 justify-content-center position-relative">
       <h2>LOTES</h2>
       <div className="lotList-table overflow-x-scroll d-flex flex-column text-admin-primary bg-admin-primary">
-        <div className='lotList-header d-flex justify-content-between fw-bold text-admin-primary bg-admin-primary'>
-            <div className='lotList-header-item col-1'>N°</div>
-            <div className='lotList-header-item col-1'>Área</div>
-            <div className='lotList-header-item col-2'>Precio</div>
-            <div className='lotList-header-item col-2'>Porcentaje de reserva</div>
-            <div className='lotList-header-item col-2'>Financiación</div>
-            <div className='lotList-header-item col-1'>Estado</div>
-            <div className='lotList-header-item col-3'>Acciones</div>
-        </div>
+        <ul className='lotList-header d-flex justify-content-between fw-bold text-admin-primary bg-admin-primary'>
+            <li className='lotList-header-item col-1'>N°</li>
+            <li className='lotList-header-item col-1'>Manzana</li>
+            <li className='lotList-header-item col-1'>Área</li>
+            <li className='lotList-header-item col-2'>Precio</li>
+            <li className='lotList-header-item col-1'>Porcentaje de reserva</li>
+            <li className='lotList-header-item col-2'>Financiación</li>
+            <li className='lotList-header-item col-1'>Estado</li>
+            <li className='lotList-header-item col-3'>Acciones</li>
+        </ul>
         <div className='lotList-content text-admin-primary bg-admin-primary'>
             {
               lots
@@ -47,11 +48,12 @@ export default function LotsList({setEditionForm,setShowForm}) {
                 return(
                   <article className='lotList-content-row d-flex justify-content-between my-3' key={i}>
                     <div className='lotList-content-item col-1 fw-bold'>{lot.number}</div>
-                    <div className='lotList-content-item col-1'>{lot.area} m2</div>
-                    <div className='lotList-content-item col-2'>USD {lot.price}</div>
-                    <div className='lotList-content-item col-2'>{lot.reservationPercentage}%</div>
-                    <div className='lotList-content-item col-2'>{lot.financiation?"Si":"No"}</div>
-                    <div className='lotList-content-item col-1'>{lot.reservationUser?<button className="btn btn-primary me-2">Reservado</button>:"Sin reservar"}</div>
+                    <div className='lotList-content-item col-1 '>{lot.block}</div>
+                    <div className='lotList-content-item col-1 '>{lot.area} m2</div>
+                    <div className='lotList-content-item col-2 '>USD {lot.price}</div>
+                    <div className='lotList-content-item col-1 '>{lot.reservationPercentage}%</div>
+                    <div className='lotList-content-item col-2 '>{lot.financiation?"Si":"No"}</div>
+                    <div className='lotList-content-item col-1 '>{lot.reservationUser?<button className="btn btn-primary me-2">Reservado</button>:"Sin reservar"}</div>
                     <div className='lotList-content-item d-flex col-3'>
                     {showPopUp&&lotToDelete===lot?
                       <DeleteConfirmation lot={lot} hidePopUp={hidePopUp}/>:
