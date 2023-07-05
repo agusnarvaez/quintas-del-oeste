797ab9c6224073f82765a32f6d9a7739418cabce
Agus Narvaez <agus.narvaez@outlook.com>
2023-07-05 09:25:06 -0300

    Implemento redirección de mapa a página de reserva de lote

--- src/components/Map/LotLocation.jsx
+++ src/components/Map/LotLocation.jsx
@@ -6,7 +6,7 @@ export default function LotLocation({lot}) {
     return(
         <>
             <Polygon  pathOptions={{color: lot.reserved?'red':'blue'}} positions={[[lot.perimeter.x1.lat,lot.perimeter.x1.lng],[lot.perimeter.x2.lat,lot.perimeter.x2.lng],[lot.perimeter.y1.lat,lot.perimeter.y1.lng],[lot.perimeter.y2.lat,lot.perimeter.y2.lng]]}/>
-            <Marker lot={lot} />
+            <Marker mapLot={lot} />
         </>
     )
 }
--- src/components/Map/Marker.jsx
+++ src/components/Map/Marker.jsx
@@ -1,21 +1,43 @@
+//* Importo el contexto de los lotes
+import { useLots } from '../../context/LotsContext'
+
+//* Importo el hook para redireccionar
+import { useNavigate } from 'react-router-dom'
+
+//* Importo los componentes de Leaflet que me permiten agregar marcadores y popups
 import {Marker, Popup } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
+import 'leaflet/dist/leaflet.css' //* Importo los estilos de Leaflet
+
+//* Importo los iconos que voy a usar para los marcadores tanto para lotes disponibles como reservados
 import IconLocationAvailable from './IconLocationAvailable'
 import IconLocationReserved from './IconLocationReserved'
 
-export default function Markers({lot}) {
-    const coordinates = {lat:lot.coordinates.lat,lng:lot.coordinates.lng}
+export default function Markers({mapLot}) {
+    //* Hook para guardar el lote seleccionado
+    const {setLot} = useLots()
+
+    //* Guardo las coordenadas del lote
+    const coordinates = {lat:mapLot.coordinates.lat,lng:mapLot.coordinates.lng}
+
+    //* Guardo el lote y redirecciono a la página de reserva
+    const navigate = useNavigate() //* Hook para redireccionar
+    const reserveLot = (lotToReserve)=>{
+        console.log(lotToReserve)
+        setLot(lotToReserve)
+        navigate("/reservar-lote")
+    }
+
     return(
-        <Marker position={coordinates} icon={lot.reserved?IconLocationReserved:IconLocationAvailable}>
+        <Marker position={coordinates} icon={mapLot.reserved?IconLocationReserved:IconLocationAvailable}>
             <Popup>
                 <ul className='list-group list-group-flush list-unstyled'>
-                    <li className='list-group-item'><b>N° de lote:</b> {lot.number}</li>
-                    <li className='list-group-item'><b>N° de Manzana:</b> {lot.block}</li>
-                    <li className='list-group-item'><b>Precio:</b> USD {lot.price}</li>
-                    <li className='list-group-item'><b>Área:</b> {lot.area}m2</li>
-                    <li className='list-group-item'><b>Precio de reserva:</b> USD {(lot.price*lot.reservationPercentage)/100}</li>
-                    {lot.financiation?<li className='list-group-item text-quintas-green fw-bold'>Con financiación</li>:<li className='list-group-item text-danger'>Sin financiación</li>}
-                    {lot.reserved?<li className='btn btn-danger'>Reservado</li>:<li className='btn btn-success bg-quintas-green border-quintas-green fw-bold'>Reservar</li>}
+                    <li className='list-group-item'><b>N° de lote:</b> {mapLot.number}</li>
+                    <li className='list-group-item'><b>N° de Manzana:</b> {mapLot.block}</li>
+                    <li className='list-group-item'><b>Precio:</b> USD {mapLot.price}</li>
+                    <li className='list-group-item'><b>Área:</b> {mapLot.area}m2</li>
+                    <li className='list-group-item'><b>Precio de reserva:</b> USD {(mapLot.price*mapLot.reservationPercentage)/100}</li>
+                    {mapLot.financiation?<li className='list-group-item text-quintas-green fw-bold'>Con financiación</li>:<li className='list-group-item text-danger'>Sin financiación</li>}
+                    {mapLot.reserved?<li className='btn btn-danger'>Reservado</li>:<li onClick={()=>reserveLot(mapLot)} className='btn btn-success bg-quintas-green border-quintas-green fw-bold'>Reservar</li>}
                 </ul>
             </Popup>
         </Marker>
