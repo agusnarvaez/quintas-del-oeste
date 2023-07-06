ed06f2c982ef9d4b24a43bd5b0fd062de882c562
Agus Narvaez <agus.narvaez@outlook.com>
2023-07-06 14:41:13 -0300

    Realizo petición a api de link de pago

--- src/api/lots.js
+++ src/api/lots.js
@@ -2,12 +2,13 @@ import axios from './axios'
 
 //* Peticiones a la API de lotes
 const apiLot = {
-    getAll: ()=>axios.get(`/lots`),
+    getAll: () => axios.get(`/lots`),
     get: id => axios.get(`/lots${id}`),
     create: lot => axios.post(`/lots/create`, lot),
     update: lot => axios.put(`/lots/update/${lot._id}`,lot),
-    reserve: (reservation) => axios.post(`/lots/reserve`,reservation),
-    delete: id => axios.delete(`/lots/delete/${id}`)
+    reserve: reservation => axios.post(`/lots/reserve`,reservation),
+    delete: id => axios.delete(`/lots/delete/${id}`),
+    createPaymentOrder: reservationData => axios.post(`/mercadoPago/create-order`, reservationData),
 }
 
 export { apiLot }
