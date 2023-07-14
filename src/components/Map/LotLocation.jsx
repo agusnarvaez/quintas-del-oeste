152f37554868793e4a901b66c6de048ea0714cb8
Agus Narvaez <agus.narvaez@outlook.com>
2023-07-13 23:16:21 -0300

    Quito contornos creados con leaflet

--- src/components/Map/LotLocation.jsx
+++ src/components/Map/LotLocation.jsx
@@ -5,7 +5,7 @@ import {Polygon } from 'react-leaflet'
 export default function LotLocation({lot}) {
     return(
         <>
-            <Polygon  pathOptions={{color: lot.reserved?'red':'blue'}} positions={[[lot.perimeter.x1.lat,lot.perimeter.x1.lng],[lot.perimeter.x2.lat,lot.perimeter.x2.lng],[lot.perimeter.y1.lat,lot.perimeter.y1.lng],[lot.perimeter.y2.lat,lot.perimeter.y2.lng]]}/>
+            {/* <Polygon  pathOptions={{color: lot.reserved?'red':'blue'}} positions={[[lot.perimeter.x1.lat,lot.perimeter.x1.lng],[lot.perimeter.x2.lat,lot.perimeter.x2.lng],[lot.perimeter.y1.lat,lot.perimeter.y1.lng],[lot.perimeter.y2.lat,lot.perimeter.y2.lng]]}/> */}
             <Marker mapLot={lot} />
         </>
     )
