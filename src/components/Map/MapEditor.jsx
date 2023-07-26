aeefb1365cc6e2e381af2ca3a1cf7183130b7470
Agus Narvaez <agus.narvaez@outlook.com>
2023-07-26 08:39:59 -0300

    Agrego mapa correcto

--- src/components/Map/MapEditor.jsx
+++ src/components/Map/MapEditor.jsx
@@ -13,7 +13,7 @@ import LotLocation from './LotLocation'
 import IconLocation from './IconLocation'
 
 //* Mapa propio
-import quintasMap from '../../assets/map/mapaQuintas.jpg'
+import quintasMap from '../../assets/map/mapaQuintas.png'
 
 //* Capa de google Maps
 import ReactLeafletGoogleLayer from 'react-leaflet-google-layer'
@@ -24,45 +24,11 @@ import credentials from '../../credentials'
 
 export default function MapEditor({setValue,getValues}) {
     const {lot,lots} = useLots()
-    const contornoExterior = [
-      [-34.61740919669242,-58.98954391479492],
-      [-34.61183817736672,-58.978638052940376],
-      [-34.60659130207151,-58.982656002044685],
-      [-34.60952613636645, -58.988385200500495],
-      [-34.61202541727224,-58.986432552337654],
-      [-34.61478167476339, -58.99168968200684]
-    ]
-
-/*     const zona1 = [
-      [-34.627649, -58.976640],
-      [-34.629395, -58.978793],
-      [-34.627331, -58.980316],
-      [-34.625900, -58.977915],
-      [-34.627649, -58.976640]
-    ]
-    const lote = [
-      [-34.627649, -58.976640],
-      [-34.628133, -58.977158],
-      [-34.627702, -58.977612],
-      [-34.627396, -58.976965],
-      [-34.627649, -58.976640]
-    ] */
 
     const _onCreate = (e) => {
       if(e.layerType==='marker'){
         setValue('coordinates.lat',e.layer._latlng.lat)
         setValue('coordinates.lng',e.layer._latlng.lng)
-      }
-      if(e.layerType==='polygon'){
-        const perimeter = e.layer.editing.latlngs[0][0]
-        setValue('perimeter.x1.lat',perimeter[0].lat)
-        setValue('perimeter.x1.lng',perimeter[0].lng)
-        setValue('perimeter.x2.lat',perimeter[1].lat)
-        setValue('perimeter.x2.lng',perimeter[1].lng)
-        setValue('perimeter.y1.lat',perimeter[2].lat)
-        setValue('perimeter.y1.lng',perimeter[2].lng)
-        setValue('perimeter.y2.lat',perimeter[3].lat)
-        setValue('perimeter.y2.lng',perimeter[3].lng)
       }
     }
 
@@ -75,51 +41,8 @@ export default function MapEditor({setValue,getValues}) {
         if(esUnMarker){
           setValue('coordinates.lat',0)
           setValue('coordinates.lng',0)
-        }else{
-          setValue('perimeter.x1.lat',0)
-          setValue('perimeter.x1.lng',0)
-          setValue('perimeter.x2.lat',0)
-          setValue('perimeter.x2.lng',0)
-          setValue('perimeter.y1.lat',0)
-          setValue('perimeter.y1.lng',0)
-          setValue('perimeter.y2.lat',0)
-          setValue('perimeter.y2.lng',0)
         }
       })
-    }
-
-    const _onMounted = drawControl => {
-      /* console.log("_onMounted", drawControl) */
-    }
-
-    const existingPolygon = ()=>{
-      const polygon ={
-        x1:{
-          lat:getValues('perimeter.x1.lat'),
-          lng:getValues('perimeter.x1.lng')
-        },
-        x2:{
-          lat:getValues('perimeter.x2.lat'),
-          lng:getValues('perimeter.x2.lng')
-        },
-        y1:{
-          lat:getValues('perimeter.y1.lat'),
-          lng:getValues('perimeter.y1.lng')
-        },
-        y2:{
-          lat:getValues('perimeter.y2.lat'),
-          lng:getValues('perimeter.y2.lng')
-        }
-      }
-
-      const conditionX1 = getValues('perimeter.x1.lat')!=="0"&&getValues('perimeter.x1.lat')!==undefined && getValues('perimeter.x1.lng')!=="0"&&getValues('perimeter.x1.lng')!==undefined
-      const conditionX2 = getValues('perimeter.x2.lat')!=="0"&&getValues('perimeter.x2.lat')!==undefined && getValues('perimeter.x2.lng')!=="0"&&getValues('perimeter.x2.lng')!==undefined
-      const conditionY1 = getValues('perimeter.y1.lat')!=="0"&&getValues('perimeter.y1.lat')!==undefined && getValues('perimeter.y1.lng')!=="0"&&getValues('perimeter.y1.lng')!==undefined
-      const conditionY2 = getValues('perimeter.y2.lat')!=="0"&&getValues('perimeter.y2.lat')!==undefined && getValues('perimeter.y2.lng')!=="0"&&getValues('perimeter.y2.lng')!==undefined
-
-      if(conditionX1&&conditionX2&&conditionY1&&conditionY2){
-        return <Polygon pathOptions={{color: 'red'}} positions={[[polygon.x1.lat,polygon.x1.lng],[polygon.x2.lat,polygon.x2.lng],[polygon.y1.lat,polygon.y1.lng],[polygon.y2.lat,polygon.y2.lng]]}/>
-      }
     }
 
     const existingMarker = ()=>{
@@ -134,56 +57,47 @@ export default function MapEditor({setValue,getValues}) {
 
     },[lot])
 
-    return (
-      <div className='map-container-admin'>
-      <MapContainer
-        center={{lat:"-34.613884",lng:"-58.982545"}}
-        zoom={15}
-        //Agregar medidas
-        style={{ width: '100%', height: '100%' }}
-        className='leaflet-container'
-        bearing='45°'
-      >
-        <ImageOverlay
-          url={quintasMap}
-          bounds={[
-            [-34.61787267051479,-58.99489995728686],
-            [-34.60686830849178, -58.97591241408306]]}
-        />
+  return (
+    <div className='map-container-admin'>
+    <MapContainer
+      center={{lat:"-34.611877785557",lng:"-58.98403511557229"}}
+      zoom={16}
+      //Agregar medidas
+      style={{ width: '100%', height: '100%' }}
+      className='leaflet-container'
+    >
       <FeatureGroup>
         <EditControl
           position='topright'
           onCreated={_onCreate}
           onEdited={_onEdited}
           onDeleted={_onDeleted}
-          onMounted={_onMounted}
-
           draw={{
             rectangle: false,
             polyline: false,
             circle: false,
             circlemarker: false,
+            polygon:false,
             marker:{
               icon:IconLocation
-            },
-            polygon:{
-              shapeOptions: {
-                guidelineDistance: 10,
-                color: "navy",
-                weight: 3
-              }
             }
           }}
         />
       </FeatureGroup>
+      <ImageOverlay
+        url={quintasMap}
+        /* [-34.61787267051479,-58.99489995728686],[-34.60686830849178, -58.97591241408306] */
+        bounds={[
+          [-34.61796764772112,-58.9950967727591],
+          [-34.60685310029432, -58.97588954690971]]}
+      />
 
-        <ReactLeafletGoogleLayer apiKey={credentials.mapsKey} type={'roadmap'} />
-      {/* <Polygon pathOptions={{color: 'gray'}} positions={contornoExterior} /> */}
-      {/* {existingPolygon()} */}
-      {existingMarker()}
-      {
-        lots.map((lotToShow,index)=> <LotLocation lot={lotToShow} key={index}/> )
-      }
+
+      <ReactLeafletGoogleLayer apiKey={credentials.mapsKey} type={'roadmap'} />
+
+      { existingMarker() }
+
+      { lots.map((lotToShow,index)=> <LotLocation lot={lotToShow} key={index}/> ) }
     </MapContainer>
     </div>
   )
