7d87c4bca8761c7818f5d9d4b8091ffff2fecc48
Agus Narvaez <agus.narvaez@outlook.com>
2023-07-27 15:55:48 -0300

    Agrego Payments vacío

---
+++ src/components/Home/Sections/Payments.jsx
@@ -0,0 +1,13 @@
+import Cercanias from './Location/CloseStreets'
+import Direccion from './Location/Address'
+
+import GoogleMap from './Location/GoogleMap'
+
+export default function Payments() {
+  return (
+    <section className="container-fluid p-0 mt-5" id="location">
+
+
+    </section>
+  )
+}
