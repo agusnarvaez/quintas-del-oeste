d239ebf6ea8839b32551b9f50a1170b040c81fd3
Agus Narvaez <agus.narvaez@outlook.com>
2023-07-27 15:55:28 -0300

    Cambio InteractiveMap por MasterPlan

--- src/components/Home/Main.jsx
+++ src/components/Home/Main.jsx
@@ -2,16 +2,14 @@ import Neighborhood from './Sections/Neighborhood'
 import Contact from './Sections/Contact'
 import MasterPlan from './Sections/MasterPlan'
 import Location from './Sections/Location'
-import InteractiveMap from '../Map/InteractiveMap'
 import Services from './Sections/Services'
 export default function Main() {
   return (
     <main data-bs-spy="scroll" data-bs-target="#navBar" tabIndex="0" data-bs-smooth-scroll="true">
       <Contact/>
       {/* <Neighborhood/> */}
-      {/* <MasterPlan/> */}
       <Services />
-      <InteractiveMap/>
+      <MasterPlan/>
       <Location/>
     </main>
   )
