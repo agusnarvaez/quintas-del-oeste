b2e00d50b4e201ad0ad39f4d34037f64d165728f
agusnarvaez <agus.narvaez@outlook.com>
2023-09-07 15:03:47 -0300

    Corrijo separación entre pasos y mapa

--- src/components/Home/Sections/MasterPlan.jsx
+++ src/components/Home/Sections/MasterPlan.jsx
@@ -3,9 +3,9 @@ import Scroll from'react-scroll'
 export default function MasterPlan() {
   var Element  = Scroll.Element
   return (
-    <Element className="container-fluid p-0 d-flex justify-content-center flex-wrap" id="masterPlan" name='masterPlan'>
+    <Element className="container-fluid p-0 px-5 d-flex justify-content-center flex-wrap" id="masterPlan" name='masterPlan'>
       <h2 className='text-quintas-green text-center'>RESERVÁ TU LOTE AHORA EN 3 PASOS</h2>
-      <div className='my-3 col-11 row flex-wrap justify-content-center  align-items-center'>
+      <div className='my-5 col-11 row flex-wrap justify-content-center  align-items-center'>
         <div className='bg-quintas-green punta-final conector-md h-50px my-2 row col-12 col-md-6 col-lg-3 p-0 px-5 px-lg-5 align-items-center justify-content-between '>
           <h3 className='text-white text-center d-flex align-items-center fs-1 col-1 m-0 p-0'>1</h3>
           <p className='text-white d-flex align-items-center col-10 m-0 p-0'>NAVEGÁ EL MASTERPLAN Y ELEGÍ TU LOTE</p>
