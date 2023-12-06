e844147d1bba738cecb048d2cd7b2d68d616b3ef
agusnarvaez <agus.narvaez@outlook.com>
2023-12-06 19:01:01 -0300

    Corrijo rutas de api

--- src/api/lots.js
+++ src/api/lots.js
@@ -3,15 +3,15 @@ import axios from './axios'
 //* Peticiones a la API de lotes
 const apiLot = {
     getAll: () => axios.get(`/lots`),
-    get: id => axios.get(`/lots${id}`),
-    create: lot => axios.post(`/lots/create`, lot),
-    update: lot => axios.put(`/lots/update/${lot._id}`,lot),
+    get: id => axios.get(`/lots/${id}`),
+    create: lot => axios.post(`/lots`, lot),
+    update: lot => axios.put(`/lots/${lot._id}`,lot),
+    delete: id => axios.delete(`/lots/${id}`),
     reserve: reservation => axios.post(`/lots/reserve`,reservation),
-    delete: id => axios.delete(`/lots/delete/${id}`),
-    createPaymentOrder: reservationData => axios.post(`/mercadoPago/create-order`, reservationData),
-    getPaymentFeedback: paymentData => axios.post(`/mercadoPago/feedback`, paymentData),
     reservations: () => axios.get(`/lots/reservations`),
     reservation: id => axios.get(`/lots/reservations/${id}`),
+    createPaymentOrder: reservationData => axios.post(`/mercadoPago/create-order`, reservationData),
+    getPaymentFeedback: paymentData => axios.post(`/mercadoPago/feedback`, paymentData),
 }
 
 export { apiLot }
