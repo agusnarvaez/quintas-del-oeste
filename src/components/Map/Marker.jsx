da3c4ebc974bd26fe30e093a7ad66377965de43f
agusnarvaez <agus.narvaez@outlook.com>
2023-12-07 17:11:01 -0300

    Implemento tamaño dinámico en marcadores

--- src/components/Map/Marker.jsx
+++ src/components/Map/Marker.jsx
@@ -5,25 +5,24 @@ import { useLots } from '../../context/LotsContext'
 import { useNavigate } from 'react-router-dom'
 
 //* Importo los componentes de Leaflet que me permiten agregar marcadores y popups
-import {Marker, Popup } from 'react-leaflet'
+import { Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css' //* Importo los estilos de Leaflet
-
-//* Importo los iconos que voy a usar para los marcadores tanto para lotes disponibles como reservados
-import IconLocationAvailable from './IconLocationAvailable'
-import IconLocationReserved from './IconLocationReserved'
 
 //* Para hacer scroll suave
 import Scroll from 'react-scroll'
 
-export default function Markers({mapLot}) {
-    //* Hook para guardar el lote seleccionado
-    const {setLot} = useLots()
-    var scroller = Scroll.animateScroll
-    //* Guardo las coordenadas del lote
-    const coordinates = {lat:mapLot.coordinates.lat,lng:mapLot.coordinates.lng}
+import {createIcon,isReserved,getIconSize} from '../../utils/mapUtils'
 
+export default function Markers({lot,size,zoom}) {
+    //* Hook para guardar el lote seleccionado
     //* Guardo el lote y redirecciono a la página de reserva
     const navigate = useNavigate() //* Hook para redireccionar
+    const {setLot} = useLots()
+    var scroller = Scroll.animateScroll
+
+    //* Guardo las coordenadas del lote
+    const coordinates = {lat:lot.coordinates.lat,lng:lot.coordinates.lng}
+
     const reserveLot = (lotToReserve)=>{
         setLot(lotToReserve)
         scroller.scrollToTop()
@@ -31,16 +30,16 @@ export default function Markers({mapLot}) {
     }
 
     return(
-        <Marker position={coordinates} icon={mapLot.reservation?IconLocationReserved:IconLocationAvailable}>
+        <Marker position={coordinates} icon={createIcon(getIconSize(zoom),isReserved(lot.reservation))}>
             <Popup>
                 <ul className='list-group list-group-flush list-unstyled'>
-                    <li className='list-group-item'><b>N° de lote:</b> {mapLot.number}</li>
-                    <li className='list-group-item'><b>N° de Manzana:</b> {mapLot.block}</li>
-                    <li className='list-group-item'><b>Precio:</b> USD {mapLot.price}</li>
-                    <li className='list-group-item'><b>Área:</b> {mapLot.area}m2</li>
+                    <li className='list-group-item'><b>N° de lote:</b> {lot.number}</li>
+                    <li className='list-group-item'><b>N° de Manzana:</b> {lot.block}</li>
+                    <li className='list-group-item'><b>Precio:</b> USD {lot.price}</li>
+                    <li className='list-group-item'><b>Área:</b> {lot.area}m2</li>
                     <li className='list-group-item'><b>Precio de reserva:</b> $ 50.000</li>
-                    {/* {mapLot.financiation?<li className='list-group-item text-quintas-green fw-bold'>Con financiación</li>:<li className='list-group-item text-danger'>Sin financiación</li>} */}
-                    {mapLot.reservation?<li className='btn btn-danger'>Reservado</li>:<li onClick={()=>reserveLot(mapLot)} className='btn btn-success bg-quintas-green border-quintas-green fw-bold'>Reservar</li>}
+                    {/* {lot.financiation?<li className='list-group-item text-quintas-green fw-bold'>Con financiación</li>:<li className='list-group-item text-danger'>Sin financiación</li>} */}
+                    {lot.reservation?<li className='btn btn-danger'>Reservado</li>:<li onClick={()=>reserveLot(lot)} className='btn btn-success bg-quintas-green border-quintas-green fw-bold'>Reservar</li>}
                 </ul>
             </Popup>
         </Marker>
