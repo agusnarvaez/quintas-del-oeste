bd1417abaca251c755ff5dbc3cec1a44b9241a2b
agusnarvaez <agus.narvaez@outlook.com>
2024-06-27 17:56:25 -0300

    Corrijo estilos de banner de contact

--- src/components/Home/Sections/Contact.jsx
+++ src/components/Home/Sections/Contact.jsx
@@ -9,30 +9,30 @@ export default function Contact() {
   return (
     <Element className="container-fluid p-0  flex-nowrap mb-3" id="contact" name='contact'>
 
-      <div className="container-fluid banner-container p-0 d-flex align-items-center justify-content-center position-relative py-lg-4 flex-wrap w-100">
+      <div className="container-fluid banner-container p-0 d-flex align-items-center justify-content-center position-relative flex-wrap w-100">
 
         {/* <div className="image-container background-image p-0 h-100 vw-100"> */}
         <img className="img-fluid background-image"src={bgContact} alt="Contact Background"/>
         {/* </div> */}
 
-        <h2 className="text-white fs-3 px-5 col-lg-6">TU LOTE ESTÁ <br/>EN GENERAL RODRIGUEZ <br/>DESCUBRÍ QUINTAS</h2>
+        <h2 className="text-white fs-2 fw-bolder text-center px-5 col-6">CALIDAD DE VIDA Y CONFORT A SOLO MINUTOS DEL CENTRO DE GENERAL RODRIGUEZ</h2>
 
        {/*  <div className="d-none d-lg-block contactForm py-lg-3 col-lg-4 bg-white-transparent" >
           <ContactForm/>
         </div> */}
       </div>
-      <div className="d-lg-none z-index-150">
+      <div className="z-index-150 my-lg-5">
         <LinksInfo/>
       </div>
 
-      <div className="container-fluid px-5 col-lg-8 my-lg-3">
+      {/* <div className="container-fluid px-5 col-lg-8 my-lg-3">
         <div className="d-none d-lg-flex justify-content-center m-lg-0 py-lg-3 col-lg-12 z-index-150" >
           <LinksInfo />
         </div>
         <h2 className="fs-3 text-quintas-green mb-lg-3">NATURALEZA, UBICACIÓN Y COMODIDAD</h2>
         <p className="fs-5 m-0">Se parte de un proyecto nuevo, un barrio cerrado de 55 hectáreas con 1000 lotes de 300 m<sup>2</sup> a 500 m<sup>2</sup>.</p>
         <p className="fs-5 fw-bold m-0">Bajas expensas, con esctrituración inmediata y seguridad.</p>
-      </div>
+      </div> */}
 
       {/* <div className="d-lg-none">
         <ContactForm/>
