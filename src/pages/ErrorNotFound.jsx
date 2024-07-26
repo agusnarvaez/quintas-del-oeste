21b5775ab4f35c02917dfd4639db6815cd75ea00
agusnarvaez <agus.narvaez@outlook.com>
2024-07-26 00:24:48 -0300

    Agrego clase y archivo de estilos de página notFound

--- src/pages/ErrorNotFound.jsx
+++ src/pages/ErrorNotFound.jsx
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom'
 import HelmetData from '../components/HelmetData'
 export default function ErrorNotFound({metaData}) {
   return (
-    <main>
+    <main className='notFoundPage'>
       <HelmetData metaData={metaData} />
       Error 404!
       <Link to="/">Volver al inicio</Link>
