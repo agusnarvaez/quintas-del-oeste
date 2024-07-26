99e65651e82f39f43a41f343580c9ecaba1a157a
agusnarvaez <agus.narvaez@outlook.com>
2024-07-25 21:36:01 -0300

    Cambio testUrl por devUrl

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import credentials from '../credentials'
 const instance = axios.create({
-    baseURL: credentials.api.testUrl,
+    baseURL: credentials.api.devUrl,
     withCredentials: true})
 
 export default instance
